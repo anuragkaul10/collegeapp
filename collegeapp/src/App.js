32c43c04eaf5a763f89a350d38758fd82290d4a0
Anurag Kaul <anuragkaul10@gmail.com>
2023-05-01 07:04:17 +0530

    updated

--- collegeapp/src/App.js
+++ collegeapp/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import Settings from "./pages/Settings";
